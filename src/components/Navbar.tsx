03e8ab13ae2a90d8adfdc8f1f71da09d4910b55b
Nagu <154983315+H277353@users.noreply.github.com>
2025-10-23 00:27:47 +0530

    many changes downgraded to next 15.5.6

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import { checkUser } from "@/lib/checkUser"
+
+export default function Navbar() {
+    console.log("hi")
+    const user = checkUser()
+    
+    return (
+        <div>
+
+        </div>
+    )
+}
